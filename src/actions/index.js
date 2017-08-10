447b591c95af43d4a7e82b67ef79afbf4886f58c
Zain Abidi <zn6420@gmail.com>
2017-08-10 00:35:57 -0700

    feat: Adding in reducers.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
 export const ADD_RECIPIE = 'ADD_RECIPIE';
 export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
 
-export const addRecipe = ({ day, recipe, meal, }) => ({
+export const addRecipe = ({ day, recipe, meal }) => ({
   type: ADD_RECIPIE,
   recipe,
   day,
-  meal,
+  meal
 });
 
-export const removeFromCalendar = ({ day, meal, }) => ({
+export const removeFromCalendar = ({ day, meal }) => ({
   type: REMOVE_FROM_CALENDAR,
   day,
-  meal,
+  meal
 });
---
+++ src/reducers/index.js
@@ -0,0 +1,72 @@
+import { ADD_RECIPIE, REMOVE_FROM_CALENDAR } from '../actions';
+
+/**
+ * The initial state for our reducer
+ * @type {Object}
+ */
+const initialCalendarState = {
+  sunday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  monday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  tuesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  wednesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  thursday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  friday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  saturday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  }
+};
+
+const calendar = (state = initialCalendarState, action) => {
+  const { day, recipe, meal } = action;
+
+  switch (action.type) {
+    case ADD_RECIPIE: {
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label
+        }
+      };
+    }
+    case REMOVE_FROM_CALENDAR: {
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null
+        }
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default calendar;
